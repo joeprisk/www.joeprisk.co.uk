fe6477a5a2e700009005d363ca87e0176b3068d2
Joe Prisk <joe.prisk@headforwards.com>
2018-04-16 14:21:26 +0100

    cms update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const gulp = require('gulp');
-const Hexo = require('hexo');
-const Twitter = require('twitter');
-const fs = require('fs');
+const gulp        = require('gulp');
+const Hexo        = require('hexo');
+const Twitter     = require('twitter');
+const fs          = require('fs');
 const runSequence = require('run-sequence');
 
 
@@ -24,25 +24,24 @@ gulp.task('generate', function (cb) {
         return hexo.call('generate', {
             watch: false
         });
-    }).then(function () {
-        return hexo.exit();
-    }).then(function () {
-        return cb()
-    }).catch(function (err) {
-        console.log(err);
-        hexo.exit(err);
-        return cb(err);
     })
+        .then(() => hexo.exit())
+        .then(() => cb())
+        .catch((err) => {
+            console.log(err);
+            hexo.exit(err);
+            return cb(err);
+        })
 })
 
 gulp.task('twitter', (cb) => {
 
     const client = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: '',//process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: ''//process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
+                                   consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+                                   consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+                                   access_token_key:    '',//process.env.TWITTER_ACCESS_TOKEN_KEY,
+                                   access_token_secret: ''//process.env.TWITTER_ACCESS_TOKEN_SECRET
+                               });
 
     let params = {screen_name: 'joeprisk', count: 5};
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -50,14 +49,14 @@ gulp.task('twitter', (cb) => {
 
             tweets = tweets.map((tweet) => {
                 return {
-                    text: tweet.text,
-                    url: "https://twitter.com/joeprisk/status/" + tweet.id_str,
+                    text:    tweet.text,
+                    url:     "https://twitter.com/joeprisk/status/" + tweet.id_str,
                     created: tweet.created_at.substring(0, tweet.created_at.length - 11),
                 };
             })
 
-            fs.writeFile('./source/_data/tweets.json', JSON.stringify(tweets), function(err) {
-                if(err) {
+            fs.writeFile('./source/_data/tweets.json', JSON.stringify(tweets), function (err) {
+                if (err) {
                     console.log(err);
                 } else {
                     console.log("Tweets data saved.");
@@ -72,7 +71,6 @@ gulp.task('redirects', (cb) => {
     console.log("Moving all static hosts files");
     gulp.src("static/*")
         .pipe(gulp.dest('dist'));
-
 
 
 });
